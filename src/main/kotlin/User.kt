947e9db0d6b2d53d9c92429f5f39600144377bc3
Gilbert Vilalva <42963600+GilbertVilalva@users.noreply.github.com>
2023-08-29 10:40:51 -0300

    Adicão de glossário

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -15,3 +15,8 @@ class User constructor(var name: String){
         return name.length
     }
 }
+
+//classe -> É o molde para criar N objetos
+//objetos -> É o conjunto de proposicções e comportamentos de um contexto do seu sistema (Button, LoginScreen, PassValidator)
+//object -> É um objeto também, só que ele é único, ou seja, não é igual a classe que sai vários obejtos
+//(exemplo: Banco de Dados, Sessão de Usuários, Logs)
